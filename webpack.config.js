85d4f496e9e68698026adaba92d888da30924aa3
baixue <baixuexue123@hotmail.com>
2017-07-15 11:23:52 +0800

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,13 @@ module.exports = {
     },
     resolve: {
         alias: {
-            'vue$': 'vue/dist/vue.esm.js'
+            'vue$': 'vue/dist/vue.esm.js',
+            'src': path.resolve(__dirname, 'src'),
+            'components': path.resolve(__dirname, '/src/components'),
+            'api': path.resolve(__dirname, '/src/api'),
+            'utils': path.resolve(__dirname, '/src/utils'),
+            'store': path.resolve(__dirname, '/src/store'),
+            'router': path.resolve(__dirname, '/src/router')
         }
     },
     devServer: {
